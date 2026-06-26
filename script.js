f48046bad543b4b1c835540dd21fbb71e7a3ae5c
atharva706 <atharvakulkarrni@gmail.com>
2026-06-26 12:03:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let student = {
+	name : "Atharva",
+	surname : "Coolkarni",
+	age :  22
+}
+
+function getKeys(student) {
+	let val = []
+		val.push(Object.keys(student))
+	return val;
+}
+	
+}
